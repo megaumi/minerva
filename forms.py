22c549bc4e7e412cae9a9c0f745983ec26ea2006
Valentina Mukhamedzhanova <umirra@gmail.com>
2011-06-22 12:58:37 +0400

    Moved forms.py to the main app dir

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django import forms
-from minerva.main.models import MagazineIssue, Article, EnglishParagraph, TranslatedParagraph
-
-class ArticleForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.Textarea)
-           
-    class Meta:
-        model = Article
